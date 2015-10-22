19db75cbab4319e0788011efc8348f873a00de88
Ginanjar Fahrul M <ginanjar.fahrul.m@gmail.com>
2015-10-22 12:55:12 +0700

    add utils classes

---
+++ PayPalApi/Utilities/Common.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PayPalApi.Utilities
+{
+    public class Common
+    {
+        public static string FormatJsonString(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+            {
+                return string.Empty;
+            }
+
+            if (json.StartsWith("["))
+            {
+                json = "{\"list\":" + json + "}";
+                var formattedText = JObject.Parse(json).ToString(Formatting.Indented);
+                formattedText = formattedText.Substring(13, formattedText.Length - 14).Replace("\n  ", "\n");
+                return formattedText;
+            }
+
+            return JObject.Parse(json).ToString(Formatting.Indented);
+        }
+
+        public static string GetRandomInvoiceNumber()
+        {
+            return new Random().Next(999999).ToString();
+        }
+    }
+}
---
+++ PayPalApi/Utilities/Configuration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PayPal.Api;
+
+namespace PayPalApi.Utilities
+{
+    public class Configuration
+    {
+        public readonly static string ClientId;
+        public readonly static string ClientSecret;
+
+        static Configuration()
+        {
+            var config = GetConfig();
+        }
+
+        public static Dictionary<string, string> GetConfig()
+        {
+            return ConfigManager.Instance.GetProperties();
+        }
+
+        private static string GetAccessToken()
+        {
+            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret, GetConfig()).GetAccessToken();
+            return accessToken;
+        }
+
+        public static APIContext GetAPIContext(string accessToken = "")
+        {
+            var apiContext = new APIContext(string.IsNullOrEmpty(accessToken) ? GetAccessToken() : accessToken);
+            apiContext.Config = GetConfig();
+            return apiContext;
+        }
+    }
+}
